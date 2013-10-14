3b9dd7ade067a02c2f8e8dbdb808392f06579165
djvansuch <djvansuch@ubuntu.(none)>
2013-10-14 15:19:29 -0600

    test #3

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,145 +1,145 @@
 class MainController < ApplicationController
 
-	before_filter except: ["login", "login_post", "root", "logout", "register", "about", "register_post"] do 
-		if session[:scholar_id] != nil
-			@scholar = Scholar.where(id: session[:scholar_id])
-		elsif @scholar == nil
-			flash[:error] = "You must be logged in to see that page."
-			#session[:original_route] = request.path_info
-			redirect_to "/login" and return
-		end
-	end
-		
-	def root
-		render :main
-	end
+  before_filter except: ["login", "login_post", "root", "logout", "register", "about", "register_post"] do 
+    if session[:scholar_id] != nil
+      @scholar = Scholar.where(id: session[:scholar_id])
+    elsif @scholar == nil
+      flash[:error] = "You must be logged in to see that page."
+      #session[:original_route] = request.path_info
+      redirect_to "/login" and return
+    end
+  end
+    
+  def root
+   render :main
+  end
 
-	def about
-		render :about
-	end
+  def about
+    render :about
+  end
 
-	def reword
-		@results = Dinosaurus.lookup(@input)[:syn]
-		render :reword
-	end
+  def reword
+    @results = Dinosaurus.lookup(@input)[:syn]
+    render :reword
+  end
 
-	def reword_post
-		@input = params["sentence"]
+  def reword_post
+    @input = params["sentence"]
 
-		if @input != ""
-			@words = @input.split(" ")
-			
-			@words.each do |word|
-				if word.length >= 3
-					lookup_result = Dinosaurus.lookup(word)
-					if lookup_result != nil 
-						adj_results = lookup_result[:adjective]
-						if adj_results != nil 
-							synonyms_adj = adj_results[:syn]
-							if synonyms_adj != nil
-								new_word = synonyms_adj.sample
-								word.replace(new_word)
-							end
-						elsif adj_results == nil
-							adverb_results = lookup_result[:adverb]
-							if adverb_results != nil
-							synonyms_adverb = adverb_results[:syn]
-								if synonyms_adverb != nil
-									new_word = synonyms_adverb.sample
-									word.replace(new_word)  
-								end 
-							end         
-						end
-					end
-					@results = @words.join(" ")
-				end
-			end
-		else 
-			flash[:error] = "Please enter a sentence you want ReWord"
-		end
-		render :reword and return
-	end
+    if @input != ""
+      @words = @input.split(" ")
+      
+      @words.each do |word|
+        if word.length >= 3
+          lookup_result = Dinosaurus.lookup(word)
+          if lookup_result != nil 
+            adj_results = lookup_result[:adjective]
+            if adj_results != nil 
+              synonyms_adj = adj_results[:syn]
+              if synonyms_adj != nil
+                new_word = synonyms_adj.sample
+                word.replace(new_word)
+              end
+            elsif adj_results == nil
+              adverb_results = lookup_result[:adverb]
+              if adverb_results != nil
+              synonyms_adverb = adverb_results[:syn]
+                if synonyms_adverb != nil
+                  new_word = synonyms_adverb.sample
+                  word.replace(new_word)  
+                end 
+              end         
+            end
+          end
+          @results = @words.join(" ")
+        end
+      end
+    else 
+      flash[:error] = "Please enter a sentence you want ReWord"
+    end
+    render :reword and return
+  end
 
-	def login
-		render :scholar_login
-	end
+  def login
+    render :scholar_login
+  end
 
-	def login_post
-		@username = params[:username]
-		@scholar = Scholar.where(username: @username).first
+  def login_post
+    @username = params[:username]
+    @scholar = Scholar.where(username: @username).first
 
-		if @username = @scholar
-			if @scholar.authenticate(params[:password]) != false
-				session[:scholar_id] = @scholar.id
-				if 
-					@scholar.is_admin == false
-					redirect_to "/index"
-				else
-					redirect_to "/admin_controller"
-				end
-			else
-				flash[:error] = "Incorrect password"
-				render :scholar_login 
-			end
-		else
-			flash[:error] = "Wrong username"
-			render :scholar_login
-		end
-	end
+    if @username = @scholar
+      if @scholar.authenticate(params[:password]) != false
+        session[:scholar_id] = @scholar.id
+        if 
+          @scholar.is_admin == false
+          redirect_to "/index"
+        else
+          redirect_to "/admin_controller"
+        end
+      else
+        flash[:error] = "Incorrect password"
+        render :scholar_login 
+      end
+    else
+      flash[:error] = "Wrong username"
+      render :scholar_login
+    end
+  end
 
-	def register
-		render :register
-	end
+  def register
+    render :register
+  end
 
-	def register_post
-		
-		if params[:commit] == "Sign Up"
-			scholar = Scholar.new
-			scholar.first_name = params["first_name"]
-			scholar.last_name = params["last_name"]
-			scholar.username = params[:username].downcase
-			scholar.password = params[:password]
-			scholar.password_confirmation = params[:password_confirmation]
-			scholar.email = params["email"]
-			if params[:student] == nil
-				scholar.student = false
-			else
-				scholar.student = true
-			end
+  def register_post
+    
+    if params[:commit] == "Sign Up"
+      scholar = Scholar.new
+      scholar.first_name = params["first_name"]
+      scholar.last_name = params["last_name"]
+      scholar.username = params[:username].downcase
+      scholar.password = params[:password]
+      scholar.password_confirmation = params[:password_confirmation]
+      scholar.email = params["email"]
+      if params[:student] == nil
+        scholar.student = false
+      else
+        scholar.student = true
+      end
 
-			if params[:is_admin] == nil
-				scholar.is_admin = false
-			else
-				scholar.is_admin = true
-			end
+      if params[:is_admin] == nil
+        scholar.is_admin = false
+      else
+        scholar.is_admin = true
+      end
 
-			scholar.save!
+      scholar.save!
 
-			if scholar.save == true
-				flash[:notice] = "Thank you for registering, please log in to start using ReWord"
-				redirect_to "/login"
-			else
-				flash[:error] = "Registration Failed"
-				render :register and return
-			end
-		end
-	end
+      if scholar.save == true
+        flash[:notice] = "Thank you for registering, please log in to start using ReWord"
+        redirect_to "/login"
+      else
+        flash[:error] = "Registration Failed"
+        render :register and return
+      end
+    end
+  end
 
-	def new_word
-		render :new_word and return
-	end
+  def new_word
+    render :new_word and return
+  end
 
-	def new_word_post
-		flash[:notice] = "Your New Word Submission has been submitted"
-		redirect_to "/new_word"
-	end
+  def new_word_post
+    flash[:notice] = "Your New Word Submission has been submitted"
+    redirect_to "/new_word"
+  end
 
-	def admin_controller
-		render :admin_control
-	end
+  def admin_controller
+    render :admin_control
+  end
 
-	def logout
-		session.clear
-		redirect_to "/login"
-	end
+  def logout
+    session.clear
+    redirect_to "/login"
+  end
 end
